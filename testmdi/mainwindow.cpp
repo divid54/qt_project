318c54314c32c983350f85d69cd5f17cd9723d97
divid54 <zxjohnson@gmail.com>
2019-05-06 10:47:41 +0800

    test mdi when webengineview added
    it lags when webengineview added to subwindow using QT 5.12.3 msvc 2017 32 bit in windows 10

---
+++ testmdi/mainwindow.cpp
@@ -0,0 +1,19 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    auto m_webView = new QWebEngineView;
+    m_webView->resize(100,100);
+    auto it = ui->mdiArea->addSubWindow(m_webView );
+    it->resize(100,100);
+    it->show();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ testmdi/mainwindow.h
@@ -0,0 +1,23 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QWebEngineView>
+#include <QMdiSubWindow>
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
